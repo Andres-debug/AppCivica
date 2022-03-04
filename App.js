484dfee2581708f3f3e8cadb5fa8d1d6033bfc1e
Andres Diaz <andresf.diazm@unac.edu.co>
2022-03-04 17:20:45 -0500

    Avatares

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import {IconButton } from 'react-native-paper'
+import { Avatar } from 'react-native-paper';
 import { StyleSheet, Text, View, Image } from 'react-native';
 import AppBar from './src/components/AppBar/AppBar';
 import { Home } from './src/views/Home';
+import { Avatares } from './src/components/Avatar/Avatar';
 
 
 export default function App() {
@@ -10,6 +12,7 @@ export default function App() {
     <View style={styles.container}>
       <Home/>
       <AppBar/>
+     <Avatares/>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
       <Image
